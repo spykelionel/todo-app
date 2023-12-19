75b139054d3b5a693f1b20554391321d33f26a64
spykelionel <social.lionel@gmail.com>
2023-12-20 00:34:12 +0100

    create ZodValidationPipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,6 @@
 import {
+  ArgumentMetadata,
+  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -6,18 +8,35 @@ import {
   Param,
   ParseIntPipe,
   Patch,
+  PipeTransform,
   Post,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
 
+import { ZodSchema } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      throw new BadRequestException('Validation failed');
+    }
+  }
+}
+
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(
+    @Body('createUserDto', ZodValidationPipe) createUserDto: CreateUserDto,
+  ) {
     return this.userService.create(createUserDto);
   }
 

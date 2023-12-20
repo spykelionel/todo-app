c02b976e0e60dced257e36e111e1cecc3ee0e4d6
spykelionel <social.lionel@gmail.com>
2023-12-20 17:02:45 +0100

    preferred zod

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,11 @@
-import { IsString } from 'class-validator';
+import { z } from 'zod';
 
-export class CreateUserDto {
-  @IsString()
-  name: string;
-  @IsString()
-  email: string;
-  @IsString()
-  password: string;
-}
+export const createUserSchema = z
+  .object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+  })
+  .required();
+
+export type CreateUserDto = z.infer<typeof createUserSchema>;

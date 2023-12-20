6adc752bdd629dd77dcbfb96d83fc7b94595ba48
spykelionel <social.lionel@gmail.com>
2023-12-20 19:39:51 +0100

    add Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

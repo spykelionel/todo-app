afcf4129c5e3762dfbaa64bbb5609be47520a4c9
spykelionel <social.lionel@gmail.com>
2023-12-20 19:41:27 +0100

    create auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() { username, password }: Record<string, string>) {
+    return this.authService.signIn(username, password);
+  }
+}

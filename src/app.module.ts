48929831c03b59b20d5f5fe709db5d1a93b3a28b
spykelionel <social.lionel@gmail.com>
2023-12-21 13:41:19 +0100

    add Todo resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, AuthModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })

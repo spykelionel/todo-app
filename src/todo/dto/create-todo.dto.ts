7592e649c3c039457eb55e0a9646b3b2875e9a94
spykelionel <social.lionel@gmail.com>
2023-12-21 13:57:18 +0100

    update create todo dto

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,1 +1,9 @@
-export class CreateTodoDto {}
+import { z } from 'zod';
+
+export const createTodoSchema = z
+  .object({
+    title: z.string(),
+  })
+  .required();
+
+export type CreateTodoDto = z.infer<typeof createTodoSchema>;

cc4a36c09419949368290bd3ef8520cf1b9dfb90
spykelionel <social.lionel@gmail.com>
2023-12-21 16:09:23 +0100

    use authorization before post a todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -6,8 +6,11 @@ import {
   Param,
   Patch,
   Post,
+  Request,
+  UseGuards,
   UsePipes,
 } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 import { ZodValidationPipe } from 'src/util/ValidationSchema';
 import { CreateTodoDto, createTodoSchema } from './dto/create-todo.dto';
 import { UpdateTodoDto, updateTodoSchema } from './dto/update-todo.dto';
@@ -18,12 +21,15 @@ export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   @UsePipes(new ZodValidationPipe(createTodoSchema))
-  create(@Body() createTodoDto: CreateTodoDto) {
-    return this.todoService.create(createTodoDto);
+  create(@Body() createTodoDto: CreateTodoDto, @Request() req) {
+    console.log(req.user);
+    return this.todoService.create(createTodoDto, req.user);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.todoService.findAll();
   }

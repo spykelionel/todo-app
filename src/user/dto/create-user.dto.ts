8eaf8f483f7646fb174a5929e779ba82a1eb13e6
spykelionel <social.lionel@gmail.com>
2023-12-23 16:13:42 +0100

    set constraints for create user DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 export const createUserSchema = z
   .object({
-    name: z.string(),
-    email: z.string(),
-    password: z.string(),
+    name: z.string().min(3, 'Name can not be less than 3 characters.'),
+    email: z.string().email('Email is not valid'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
   })
   .required();
 
